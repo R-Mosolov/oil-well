8e80e45c15ff229d759d52bb71366635e7795a28
Roman Mosolov <R.V.Mosolov@ya.ru>
2021-01-23 13:44:38 +0300

    Add simple realization of changes by scrolling

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const customGrey = "rgba(0, 0, 0, 0.20)";
+let colorLightness = 20;
+const customGrey = `rgba(0, 0, 0, 0.${colorLightness})`;
 
 ctx.fillStyle = customGrey;
 ctx.fillRect(650, 10, 100, 850);
+
+window.addEventListener('scroll', function() {
+  document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
+});
