532d08b65ce1caed0c412854414210104fbb9fbd
Roman Mosolov <R.V.Mosolov@ya.ru>
2021-01-23 13:36:22 +0300

    Add simple realization of container

---
+++ app.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const customGrey = "rgba(0, 0, 0, 0.20)";
+
+ctx.fillStyle = customGrey;
+ctx.fillRect(650, 10, 100, 850);
